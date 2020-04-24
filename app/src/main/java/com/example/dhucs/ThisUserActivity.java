6da34d84d6f455bacf7540734e8af4617b0880cd
admin <710278488@qq.com>
2020-04-24 14:59:41 +0800

    志愿者完结

--- app/src/main/java/com/example/dhucs/ThisUserActivity.java
+++ app/src/main/java/com/example/dhucs/ThisUserActivity.java
@@ -1,10 +1,12 @@
 package com.example.dhucs;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -22,8 +24,10 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Base64;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -45,24 +49,20 @@ import java.util.concurrent.TimeUnit;
 /**
  * 查看报名用户
  */
-public class ThisUserActivity extends AppCompatActivity
-{
+public class ThisUserActivity extends AppCompatActivity {
     private ImageView haveaacback;
     private RecyclerView recyvle;
     private SwipeRefreshLayout swipeRefreshLayout;
     private List<User> userList = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_this_user);
         haveaacback = findViewById(R.id.haveacbacks);
-        haveaacback.setOnClickListener(new View.OnClickListener()
-        {
+        haveaacback.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 finish();
             }
         });
@@ -70,11 +70,9 @@ public class ThisUserActivity extends AppCompatActivity
         userList = (List<User>) getIntent().getSerializableExtra("userList");
         requestData();
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
-        {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onRefresh()
-            {
+            public void onRefresh() {
                 //这里获取数据的逻辑
                 requestData();
                 swipeRefreshLayout.setRefreshing(false);
@@ -84,18 +82,27 @@ public class ThisUserActivity extends AppCompatActivity
         recyvle.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         initData();
         recyvle.setAdapter(adapter);
-//        adapter.setOnItemClickListener(new OnItemClickListener()
-//        {
-//            @Override
-//            public void onItemClick(@NonNull int position)
-//            {
-//                final User activities = activitiesList.get(position);
-//                startActivity(new Intent(ThisUserActivity.this, AddActivity.class).putExtra("title", activities.getName())
-//                        .putExtra("img", activities.getImage())
-//                        .putExtra("content", activities.getAccount())
-//                        .putExtra("ids", activities.getId()));
-//            }
-//        });
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(@NonNull int position) {
+                final User activities = activitiesList.get(position);
+                LayoutInflater layoutInflater = LayoutInflater.from(ThisUserActivity.this);
+                View newPlanDialog = layoutInflater.inflate(R.layout.baoming_dia, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(ThisUserActivity.this);
+                EditText ac_time = newPlanDialog.findViewById(R.id.ac_time);
+                EditText ac_date = newPlanDialog.findViewById(R.id.ac_date);
+                ac_time.setEnabled(false);
+                ac_date.setEnabled(false);
+                if (activities.getWorkLong() != null && !activities.getWorkLong().equals(""))
+                    ac_time.setText(activities.getWorkLong());
+                if (activities.getTimeLong() != null && !activities.getTimeLong().equals(""))
+                    ac_date.setText(activities.getTimeLong());
+                builder.setTitle("提示")
+                        .setView(newPlanDialog)
+                        .setPositiveButton("确定", null)
+                        .show();
+            }
+        });
     }
 
     private BaseRecyclerAdapter adapter;
@@ -103,51 +110,39 @@ public class ThisUserActivity extends AppCompatActivity
     protected MaterialDialog loadingDialog;
 
 
-    private void initData()
-    {
-        adapter = new BaseRecyclerAdapter()
-        {
+    private void initData() {
+        adapter = new BaseRecyclerAdapter() {
             @Override
-            protected void onBindView(@NonNull BaseViewHolder holder, @NonNull final int position)
-            {
+            protected void onBindView(@NonNull BaseViewHolder holder, @NonNull final int position) {
                 final User activities = activitiesList.get(position);
                 ImageView item_img = holder.getView(R.id.item_img);
                 TextView item_title = holder.getView(R.id.item_title);
                 TextView item_new = holder.getView(R.id.item_new);
                 Button item_pass = holder.getView(R.id.item_pass);
                 Button item_setting = holder.getView(R.id.item_setting);
-                if (activities.getAccess() != null)
-                {//通过集合
-                    if (activities.getAccess())
-                    {
+                if (activities.getAccess() != null) {//通过集合
+                    if (activities.getAccess()) {
                         item_pass.setText("已通过");
-                        if (activities.getActivityAdmin() != null)
-                        {//是否是管理员
-                            if (activities.getActivityAdmin())
-                            {
+                        if (activities.getActivityAdmin() != null) {//是否是管理员
+                            if (activities.getActivityAdmin()) {
 
                                 item_setting.setText("取消管理");
                             }
-                        } else
-                        {
+                        } else {
 
                             item_setting.setText("设置管理");
                         }
-                    } else
-                    {
+                    } else {
                         item_pass.setText("通过");
                     }
 
-                } else
-                {
+                } else {
                     item_pass.setText("通过");
                     item_setting.setText("设置管理");
                 }
-                item_pass.setOnClickListener(new View.OnClickListener()
-                {// 通过
+                item_pass.setOnClickListener(new View.OnClickListener() {// 通过
                     @Override
-                    public void onClick(View v)
-                    {
+                    public void onClick(View v) {
                         if (item_pass.getText().toString().equals("已通过"))
                             Toast.makeText(ThisUserActivity.this, "不能重复通过", Toast.LENGTH_SHORT).show();
                         else
@@ -155,54 +150,44 @@ public class ThisUserActivity extends AppCompatActivity
                     }
                 });
 
-                item_setting.setOnClickListener(new View.OnClickListener()
-                {//取消管理员||设置管理员
+                item_setting.setOnClickListener(new View.OnClickListener() {//取消管理员||设置管理员
                     @Override
-                    public void onClick(View v)
-                    {
-                        if (item_pass.getText().toString().contains("已"))
-                        {
+                    public void onClick(View v) {
+                        if (item_pass.getText().toString().contains("已")) {
                             if (item_setting.getText().toString().contains("设置"))
                                 requestGuanLi(activities);
                             else if (item_setting.getText().toString().equals("取消管理"))
                                 requestCancel(activities);
-                        } else
-
-                        {
+                        } else {
                             Toast.makeText(ThisUserActivity.this, "请先通过审核", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
                 item_title.setText(activities.getName());
                 item_new.setText(activities.getAccount());
-                if (!activities.getImage().equals(""))
-                {
+                if (!activities.getImage().equals("")) {
                     item_img.setVisibility(View.VISIBLE);
                     byte[] decodedString = Base64.decode(activities.getImage().substring(activities.getImage().indexOf(",") + 1), Base64.DEFAULT);
                     Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
                     item_img.setImageBitmap(decodedByte);
-                } else
-                {
+                } else {
                     item_img.setVisibility(View.GONE);
                 }
             }
 
             @Override
-            protected int getLayoutResId(int position)
-            {
+            protected int getLayoutResId(int position) {
                 return R.layout.item_audit;
             }
 
             @Override
-            public int getItemCount()
-            {
+            public int getItemCount() {
                 return activitiesList.size();
             }
         };
     }
 
-    private void requestCancel(User user)
-    {
+    private void requestCancel(User user) {
         showLoadingDialog();
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(10, TimeUnit.SECONDS)
@@ -219,17 +204,14 @@ public class ThisUserActivity extends AppCompatActivity
                 .post(requestBody)
                 .build();
         Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback()
-        {
+        call.enqueue(new Callback() {
             @Override
-            public void onFailure(Call call, IOException e)
-            {
+            public void onFailure(Call call, IOException e) {
                 Log.e("error", "connectFail");
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException
-            {
+            public void onResponse(Call call, Response response) throws IOException {
                 Message msg = new Message();
                 msg.what = 4;
                 msg.obj = response.body().string();
@@ -239,8 +221,7 @@ public class ThisUserActivity extends AppCompatActivity
         });
     }
 
-    private void requestTongguo(User user)
-    {
+    private void requestTongguo(User user) {
         showLoadingDialog();
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(10, TimeUnit.SECONDS)
@@ -263,17 +244,14 @@ public class ThisUserActivity extends AppCompatActivity
                 .post(requestBody)
                 .build();
         Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback()
-        {
+        call.enqueue(new Callback() {
             @Override
-            public void onFailure(Call call, IOException e)
-            {
+            public void onFailure(Call call, IOException e) {
                 Log.e("error", "connectFail");
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException
-            {
+            public void onResponse(Call call, Response response) throws IOException {
                 Message msg = new Message();
                 msg.what = 3;
                 msg.obj = response.body().string();
@@ -283,50 +261,40 @@ public class ThisUserActivity extends AppCompatActivity
         });
     }
 
-    private Handler mHandler = new Handler(new Handler.Callback()
-    {
+    private Handler mHandler = new Handler(new Handler.Callback() {
         @Override
-        public boolean handleMessage(@NonNull Message msg)
-        {
-            switch (msg.what)
-            {
+        public boolean handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
                 case 1:
                     String string = (String) msg.obj;
                     Gson gson = new Gson();
                     Log.e("返回用户", string);
-                    activitiesList = gson.fromJson(string, new TypeToken<List<User>>()
-                    {
+                    activitiesList = gson.fromJson(string, new TypeToken<List<User>>() {
                     }.getType());
                     adapter.notifyDataSetChanged();
                     break;
                 case 2:
-                    if (!Boolean.parseBoolean(msg.obj.toString()))
-                    {
+                    if (!Boolean.parseBoolean(msg.obj.toString())) {
                         Toast.makeText(ThisUserActivity.this, "设置管理员失败", Toast.LENGTH_SHORT).show();
-                    } else
-                    {
+                    } else {
                         Toast.makeText(ThisUserActivity.this, "设置管理员成功", Toast.LENGTH_SHORT).show();
                         finish();
                     }
 
                     break;
                 case 3:
-                    if (!Boolean.parseBoolean(msg.obj.toString()))
-                    {
+                    if (!Boolean.parseBoolean(msg.obj.toString())) {
                         Toast.makeText(ThisUserActivity.this, "审核失败", Toast.LENGTH_SHORT).show();
-                    } else
-                    {
+                    } else {
                         Toast.makeText(ThisUserActivity.this, "审核成功", Toast.LENGTH_SHORT).show();
                         finish();
                     }
 
                     break;
                 case 4:
-                    if (!Boolean.parseBoolean(msg.obj.toString()))
-                    {
+                    if (!Boolean.parseBoolean(msg.obj.toString())) {
                         Toast.makeText(ThisUserActivity.this, "取消管理员失败", Toast.LENGTH_SHORT).show();
-                    } else
-                    {
+                    } else {
                         Toast.makeText(ThisUserActivity.this, "取消管理员成功", Toast.LENGTH_SHORT).show();
 //                        requestData();
                         finish();
@@ -338,8 +306,7 @@ public class ThisUserActivity extends AppCompatActivity
         }
     });
 
-    private void requestData()
-    {
+    private void requestData() {
 
         showLoadingDialog();
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
@@ -356,17 +323,14 @@ public class ThisUserActivity extends AppCompatActivity
                 .post(requestBody)
                 .build();
         Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback()
-        {
+        call.enqueue(new Callback() {
             @Override
-            public void onFailure(Call call, IOException e)
-            {
+            public void onFailure(Call call, IOException e) {
                 Log.e("error", "connectFail");
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException
-            {
+            public void onResponse(Call call, Response response) throws IOException {
                 Message msg = new Message();
                 msg.what = 1;
                 msg.obj = response.body().string();
@@ -381,8 +345,7 @@ public class ThisUserActivity extends AppCompatActivity
      *
      * @param user
      */
-    private void requestGuanLi(User user)
-    {
+    private void requestGuanLi(User user) {
 
         showLoadingDialog();
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
@@ -401,17 +364,14 @@ public class ThisUserActivity extends AppCompatActivity
                 .post(requestBody)
                 .build();
         Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback()
-        {
+        call.enqueue(new Callback() {
             @Override
-            public void onFailure(Call call, IOException e)
-            {
+            public void onFailure(Call call, IOException e) {
                 Log.e("error", "connectFail");
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException
-            {
+            public void onResponse(Call call, Response response) throws IOException {
                 Message msg = new Message();
                 msg.what = 2;
                 msg.obj = response.body().string();
@@ -421,11 +381,9 @@ public class ThisUserActivity extends AppCompatActivity
         });
     }
 
-    protected void showLoadingDialog()
-    {
+    protected void showLoadingDialog() {
         loadingDialog = new MaterialDialog.Builder(this).content(R.string.loading).progress(true, 0).build();
-        if (loadingDialog != null && !loadingDialog.isShowing())
-        {
+        if (loadingDialog != null && !loadingDialog.isShowing()) {
             loadingDialog.show();
         }
     }
@@ -433,10 +391,8 @@ public class ThisUserActivity extends AppCompatActivity
     /**
      * 隐藏loading对话框
      */
-    protected void dismissLoadingDialog()
-    {
-        if (loadingDialog != null && loadingDialog.isShowing())
-        {
+    protected void dismissLoadingDialog() {
+        if (loadingDialog != null && loadingDialog.isShowing()) {
             loadingDialog.dismiss();
         }
     }
